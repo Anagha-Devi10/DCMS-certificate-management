7f83e6fb1ed1b8dd1c26c80fb648707df0ba0d2b
Aleena P Mathews <aleenapmathews2004@gmail.com>
2024-10-26 10:38:12 +0530

    added logo,name,change in nav bar

--- digital-certificate-management/src/components/Header.js
+++ digital-certificate-management/src/components/Header.js
@@ -1,21 +1,40 @@
 // src/components/Header.js
-import React from 'react';
+import React from "react";
 // import './Components.css'; // Common component-specific styles
-import styles from './header.module.css'
+import styles from "./header.module.css";
+import { Link, useLocation } from "react-router-dom";
 
 function Header() {
+  const location = useLocation();
+  console.log(location.pathname);
+  let isHome = false;
+  if (location.pathname == "/") {
+    isHome = true;
+  }
+
   return (
-    <header className= {styles["header"]}>
+    <header className={styles["header"]}>
       <div className={styles["container"]}>
-        <h1>Welcome to the Digital Certificate Management System</h1>
+        <div className={styles["branding"]}>
+          <Link to="/">
+            <img className={styles["logo"]} src="/logo.svg" alt="Logo" />
+          </Link>
+        </div>
         <nav className={styles["nav"]}>
-          <a href="/dashboard">Dashboard</a>
-          <a href="/manage-certificates">Manage Certificates</a>
-          <a href="/renewal-requests">Renewal Requests</a>
-          <a href="/reports">Reports</a>
-          <a href="/login" className="login-btn">Login</a>
+          <Link to="/dashboard">Dashboard</Link>
+          <Link to="/manage-certificates">Manage Certificates</Link>
+          <Link to="/renewal-requests">Renewal Requests</Link>
+          <Link to="/reports">Reports</Link>
+          <Link to="/login" className="login-btn">
+            Login
+          </Link>
         </nav>
       </div>
+      {isHome ? (
+        <h1 className={styles["welcome-text"]}>
+          Welcome to the Digital Certificate Management System
+        </h1>
+      ) : null}
     </header>
   );
 }
